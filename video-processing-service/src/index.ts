01dc79dff3f35244eebcbe5113760e36b189e645
darkevergard3n <darkevergarden@gmail.com>
2025-05-19 09:40:38 +0700

    done changing the video-processing-service

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,30 +1,40 @@
 import express from "express";
+import { Request, Response } from "express";
 import ffmpeg from "fluent-ffmpeg";
 
 
 const app = express();
-const port = 3000;
+app.use(express.json());   
 
-app.post("/process-video", (req, res) => {
+app.post("/process-video", (req: Request, res: Response) => {
     //Get path of the input video fule from request body
     const inputFilePath = req.body.inputFilePath;
     const outputFilePath = req.body.outputFilePath;
 
+    //check of the input file path and output file path are provided
+    //if not provided, return a 400 Bad Request response
+    //if provided, process the video
+    //and return a 200 OK response
     if (!inputFilePath || !outputFilePath) {
         return res.status(400).send("Input and output file paths are required");
     }
 
+    //create a new ffmpeg command 
     ffmpeg(inputFilePath)
-        .outputOptions("-vf", "scale=-1:360") // conver 360p
-        .on("end", () => {
+    .outputOptions('-vf', 'scale=-1:360') // 360p
+    .on('end', function() {
+        console.log('Processing finished successfully');
+        res.status(200).send('Processing finished successfully');
+    })
+    .on('error', function(err: any) {
+        console.log('An error occurred: ' + err.message);
+        res.status(500).send('An error occurred: ' + err.message);
+    })
+    .save(outputFilePath);
+});
 
-        })
-        .on("error" , (err) => {
-            console.log(`An error occurred: ${err.message}`);
-            res.status(500).send(`Internal Server Error: ${err.message}`);
-        });
-        .save(outputFilePath);
 
+const port = process.env.PORT || 3000;
 app.listen(port,() =>{
     console.log(
         `Video Processing service listening at http://localhost:${port}`);
